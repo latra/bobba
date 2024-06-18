ca926c891aa1807a66f270b9cba87f796ba9f89d
Paula Gallucci <paula.gallucci@eurecat.org>
2024-06-18 14:22:30 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('credit-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+    alert('Felicidades, has desbloqueado la full experiencia original');
+});
